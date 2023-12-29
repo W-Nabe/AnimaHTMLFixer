e78862ae7d8cc46bc31e0b653a75ebc3423ede41
watanabe <nabe@lean-system.co.jp>
2023-12-29 10:57:04 +0900

    PHPエラー表示・非表示コメント追加

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-// error_reporting(0); // デバッグ時はこの行をコメントアウト
+// ini_set( "display_errors", 1 ); // エラーを強制的に表示したいときはこの行をコメントアウト
+// error_reporting(E_ALL); // エラーを強制的に表示したいときはこの行をコメントアウト
+// error_reporting(0); // エラーを強制的に非表示したいときはこの行をコメントアウト
 require_once("_wp_variables.inc");
 require_once("_check-ua.inc"); // ブラウザ横幅のチェックとDOCTYPE,html,headタグ配置
 
 if (isset($_COOKIE["windowWidth"])) require_once("./_operate_views.inc");
-
 // DOMをhtmlとして表示
 echo output($dom);
 ?>
